455c81581bc7c0f1d77eda204207d1a7939188b4
Astrodelics <mikecurran0000@gmail.com>
2022-10-24 15:19:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#yourname = input("what is your name")
+#print("hello", yourname)
+#your_class = input("what is your favorite class:")
+#loud_version = your_class.upper()
+#print("YOUR FAVORITE CLASS IS", loud_version)
+
+#class_name = input("what class are you taking next semerster:")
+#num_credits = input("how many credits is it:")
+#print(f"You are taking {class_name} next semster for {num_credits} credits")
+
+#courses = ["math", "science", "history", "reading"]
+#courses.append("comp151")
+#print(courses)
+
+#courses = ["math", "science", "history", "reading"]
+#courses.insert(2, "comp151")
+#print(courses)
+
+#my_file = open("fun.txt", "r")
+#all_lines = my_file.readlines()
+#for line in all_lines:
+#   print(line)
+#   print("will show after each line")
+#print("this will print once after")
